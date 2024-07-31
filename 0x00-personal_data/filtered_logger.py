4ffab2e756f292393f11b7bc1b2839511c4789d0
godfreydekew <dekewgodfrey@gmail.com>
2024-07-31 13:49:30 +0000

    Task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """0. Regex-ing"""
 import re
 from typing import List
+import logging
 
 
 def filter_datum(
@@ -13,3 +14,24 @@ def filter_datum(
     pattern = f"({'|'.join(fields)})=.*?{separator}"
     return re.sub(
             pattern, lambda m: f"{m.group(1)}={redaction}{separator}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Formats the logg data and hides private info"""
+        message = super().format(record)
+        for field in self.fields:
+            message = re.sub(
+                    rf'{field}=[^;]*', f'{field}={self.REDACTION}', message)
+        return message
