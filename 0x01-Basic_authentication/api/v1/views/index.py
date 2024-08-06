7fdeb1dd9a12e37a0bb976aa374bd2ce180ce82b
godfreydekew <dekewgodfrey@gmail.com>
2024-08-06 19:49:08 +0000

    Task 2

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,6 +26,14 @@ def stats() -> str:
     return jsonify(stats)
 
 
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """GET /api/v1/forbidden
+    This endpoint must raise a 403 error by using abort
+    """
+    abort(403)
+
+
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """ GET /api/v1/unauthorized
