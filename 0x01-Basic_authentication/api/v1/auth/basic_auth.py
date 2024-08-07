5ef25dee01be2c0b7c51e5ad8444bdf7a9beb932
godfreydekew <dekewgodfrey@gmail.com>
2024-08-07 16:47:01 +0300

    Task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 """6. Basic auth"""
+import base64
+import binascii
+
 from api.v1.auth.auth import Auth
 
 
@@ -23,3 +26,21 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str
+    ) -> str:
+        """returns the decoded value of a
+         Base64 string base64_authorization_header
+        """
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+
+        try:
+            data = base64.b64decode(base64_authorization_header)
+            return data.decode('utf-8')
+        except binascii.Error as e:
+            return None
