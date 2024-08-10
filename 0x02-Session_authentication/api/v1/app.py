ea33a0dbd4d3d47de0cd12596ea5c421b0e1b75f
godfreydekew <dekewgodfrey@gmail.com>
2024-08-10 12:27:21 +0300

    Task 5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -51,11 +51,15 @@ def before_request():
     if auth:
         excluded_paths = ['/api/v1/status/',
                           '/api/v1/unauthorized/',
-                          '/api/v1/forbidden/'
+                          '/api/v1/forbidden/',
+                          '/api/v1/auth_session/login/'
                           ]
 
         if auth.require_auth(request.path, excluded_paths):
-            if auth.authorization_header(request) is None:
+            # if auth.authorization_header(request) is None:
+            #     abort(401)
+            if (not auth.authorization_header(request)
+                    and not auth.session_cookie(request)):
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
