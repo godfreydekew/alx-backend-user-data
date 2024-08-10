2a60a36246c7e13960905acc57b4a908789dd0c1
godfreydekew <dekewgodfrey@gmail.com>
2024-08-10 11:36:33 +0300

    Update

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,11 +3,11 @@
 from api.v1.auth.auth import Auth
 import uuid
 
+
 class SessionAuth(Auth):
     """A class to manage session authentication"""
 
-    def __init__(self):
-        self.user_id_by_session_id = {}
+    user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
         """ creates a Session ID for a user_id"""
@@ -17,5 +17,5 @@ class SessionAuth(Auth):
             return None
 
         session_id = str(uuid.uuid4())
-        self.user_id_by_session_id[session_id] = user_id
+        SessionAuth.user_id_by_session_id[session_id] = user_id
         return session_id
