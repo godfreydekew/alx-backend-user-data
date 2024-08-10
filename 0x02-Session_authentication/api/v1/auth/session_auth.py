295be17ef01d52f80bd1ce3cee8f874f1ac1d99e
godfreydekew <dekewgodfrey@gmail.com>
2024-08-10 13:34:31 +0300

    Task 6

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """1. Empty session"""
 from api.v1.auth.auth import Auth
 import uuid
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -21,9 +22,17 @@ class SessionAuth(Auth):
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """Returns User ID based on a Session ID"""
+        """Returns User session ID based on a Session ID"""
         if session_id is None:
             return None
         if not isinstance(session_id, str):
             return None
-        return SessionAuth.user_id_by_session_id.get(session_id)
+        return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        session_id = self.session_cookie(request)
+        if session_id:
+            user_id = self.user_id_for_session_id(session_id)
+            if user_id:
+                user = User.get(user_id)
+                return user
