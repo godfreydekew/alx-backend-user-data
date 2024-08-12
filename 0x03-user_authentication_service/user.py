5aa62dbc70ca5d793078b76fe34d53b3293489d6
godfreydekew <dekewgodfrey@gmail.com>
2024-08-12 19:37:10 +0300

    Task 0

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""0. User model"""
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine("sqlite://", echo=True)
+Base = declarative_base()
+
+
+class User(Base):
+    """Defines class for users"""
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
