3365528c357e0d1afb9d6fa0bb55aa8b0ebbaf96
godfreydekew <dekewgodfrey@gmail.com>
2024-08-13 19:40:13 +0300

    Check if password is None

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,11 +3,14 @@
 import bcrypt
 
 
-def _hash_password(password: str):
+def _hash_password(password: str) -> bytes:
     """
     returned bytes is a salted hash of the input
     password, hashed with bcrypt.hashpw
     """
+    if password is None:
+        raise ValueError("Password must not be None")
+
     salt = bcrypt.gensalt()
     password_in_bytes = password.encode('utf-8')
     hash_password = bcrypt.hashpw(
