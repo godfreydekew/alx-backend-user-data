578bea340ef141c867b7d8e05964cedb30d2108a
godfreydekew <dekewgodfrey@gmail.com>
2024-08-14 11:03:53 +0300

    Update

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -4,8 +4,9 @@ import flask
 from flask import Flask, request
 from auth import Auth
 
-AUTH = Auth()
+
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/")
@@ -16,14 +17,13 @@ def home():
 
 @app.route("/users", methods=['POST'])
 def users():
-    """Post request for users"""
+    """Checks if the user with the given email is created already or not"""
     email = request.form.get('email')
     password = request.form.get('password')
     try:
         user = AUTH.register_user(email, password)
-        return flask.jsonify({"email": email, "message": password})
-
-    except Exception:
+        return flask.jsonify({"email": email, "message": "user created"})
+    except ValueError as err:
         return flask.jsonify({"message": "email already registered"}), 400
 
 
