cefe001e0f4767231c0dce5580a1b6447923aef5
godfreydekew <dekewgodfrey@gmail.com>
2024-08-15 12:59:37 +0300

    Task 19

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -76,5 +76,22 @@ def get_reset_password_token():
         abort(403)
 
 
+@app.route("/reset_password", methods=['PUT'], strict_slashes=False)
+def update_password():
+    email = request.form.get('email')
+    password = request.form.get('new_password')
+    reset_token = request.form.get('reset_token')
+
+    if not email or not reset_token or not password:
+        abort(400)
+
+    try:
+        AUTH.update_password(reset_token, password)
+        return flask.jsonify(
+            {"email": email, "message": "Password updated"}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
